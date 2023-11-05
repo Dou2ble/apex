186307f144e57ec95c43cb33cd196bf726db272b
Dou2ble <22otro@stockholmscience.se>
2023-11-05 16:37:27 +0100

    fixed curl package

--- packages/curl/pkg.sh
+++ packages/curl/pkg.sh
@@ -3,11 +3,17 @@
 VERSION="7.85.0"
 SRC="https://curl.se/download/curl-${VERSION}.tar.bz2"
 
-curl -o curl.tar.bz2 $SRC
+# curl -o curl.tar.bz2 $SRC
 tar xvf curl.tar.bz2
 
 cd curl-$VERSION
 
+  ./configure --prefix=/usr \
+              --with-openssl
+  make -j$(nproc)
 
+  make DESTDIR=$(realpath ../pkg) install
 
 cd ..
+
+rm -rv curl-$VERSION curl.tar.bz2
