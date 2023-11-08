f7377288892936a526d16c8f372ef3810114f31a
Dou2ble <22otro@stockholmscience.se>
2023-11-08 16:36:10 +0100

    added gnu-make-without-guile

--- 000-wip-packages/linux-lts515/pkg.sh
+++ 000-wip-packages/linux-lts515/pkg.sh
@@ -4,7 +4,13 @@ VERSION="5.15.137"
 SRC="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${VERSION}.tar.xz"
 
 curl -o linux.tar.xz $SRC
-
 tar xvf linux.tar.xz
 
 cd linux-$VER
+
+  make mrproper
+  make defconfig
+
+
+
+cd ..
